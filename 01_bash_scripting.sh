7772ade59ae4af8b3d0ff066a5a492b2bef60bee
larinpaul <www.larinpavel@mail.ru>
2024-12-31 08:20:01 +0300

    What Bash is, its history and what Bash Scripts are used for

---
+++ 01_bash_scripting.sh
@@ -0,0 +1,27 @@
+# https://www.geeksforgeeks.org/bash-scripting-introduction-to-bash-and-bash-scripting/
+
+# Bash is a command-line interpreter or Unix Shell and it is 
+# widely used in GNU/Linux Operating System.
+# It is written by Brian Jhan Fox.
+# It is used as a default login shell for most Linux distributions.
+# Scripting is used to automate the execution of the tasks
+# so that humans do not need to perform them individuall.y
+# Bash scripting is a great way to automate different types of tasks in a system.
+# Developers can avoid doing repetitive tasks using bash scripting.
+
+# Bash scripting supports variables, conditional statements, and loops 
+# just like programming languages.
+# Below are some of the applications of Bash Scripts -
+
+# Table of Content
+# * Applications of Bash Scripts:
+# * Advantages of Bash Scripts:
+# * How to Write Bash Scripts?
+# * File Names and Permissions
+# * Variables
+# * Input and Output
+# * Functions
+# * Decision Making
+# * String and Numeric Comparisons
+
+
